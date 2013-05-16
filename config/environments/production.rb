7363f03edd42afa9a07cfe51ca92997c32e67185
Fernando Junior <fernandoasfj@gmail.com>
2013-05-16 16:37:02 -0300

    fjunior fixed deploy configuration
    fjunior fixed pg gem file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ CorposPercurssivos::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.compile = true
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
