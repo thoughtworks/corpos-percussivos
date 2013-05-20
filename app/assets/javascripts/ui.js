52dcef1ce2b1011e99573703e1eee58ed9442d9a
Ricardo Wendell <rwendel@thoughtworks.com>
2013-05-20 17:57:17 -0300

    Configuring i18n for the site

--- app/assets/javascripts/ui.js
+++ app/assets/javascripts/ui.js
@@ -1,4 +1,4 @@
 function scrollToAnchor(aid){
     var aTag = $("a[name='"+ aid +"']");
-    $('body').animate({scrollTop: aTag.offset().top - 50});
+    $('body').animate({scrollTop: aTag.offset().top - 50}, 'slow');
 }
